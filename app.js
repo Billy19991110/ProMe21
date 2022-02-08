b719672d12421883435a08b709e88ad06a569205
Billy19991110 <billy24681014@gmail.com>
2022-02-08 12:05:17 +0800

    20220208_v1

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ var express = require('express');
 var app = express();
 
 app.listen(3000);
+app.use(express.static('public'));
 
 var mysql = require('mysql');
 
 var conn = mysql.createConnection({
+    multipleStatements: true,
+
     database: 'test_v1',
     host: 'localhost',
     port: 3306,
@@ -19,19 +22,19 @@ conn.connect(function (err) {
 
 ///////////////////網頁首頁/////////////////////
 app.get('/', function (req, res) {
-    conn.query('SELECT * FROM `nation`',
+    conn.query('SELECT * FROM `nation` ; SELECT * FROM `product` JOIN `picture` ON `product`.`productID` = `picture`.`productID` JOIN `class` ON `product`.`classID` = `class`.`classID`',
         function (err, result) {
-            // nation = result
-            res.render('index.ejs', { result });
+            res.render('index.ejs', { nation: result[0], product: result[1] });
         });
-    /* conn.query('SELECT * FROM `product`',
-        function (err, result) {
-            product = result
-        }); */
 
 })
 //////////////////網頁首頁/////////////////////
 
-//////////////拿國家資料/////////////////
-app.get('/nation', function (req, res) {
+//////////////所有商品頁/////////////////
+app.get('/product', function (req, res) {
+    conn.query('SELECT * FROM `product`',
+        function (err, result) {
+            // product = result
+            res.render('product.ejs', { result });
+        });
 })
