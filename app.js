54b498b1346c19ea8c0025f252169f38e58ce4e4
Billy19991110 <billy24681014@gmail.com>
2022-02-21 21:55:46 +0800

    0221_VVV

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ var app = express();
 app.listen(3000);
 app.use(express.static('public'));
 
+/* var bodyparser = require('bodyparser');
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(bodyparser.json()); */
+
 var mysql = require('mysql');
 
 var conn = mysql.createConnection({
@@ -23,11 +27,10 @@ conn.connect(function (err) {
 
 ////////////////網頁首頁//////////////////
 app.get('/', function (req, res) {
-    conn.query('SELECT * FROM `nation` ; SELECT * FROM `product` JOIN `picture` ON `product`.`productID` = `picture`.`productID` JOIN `class` ON `product`.`classID` = `class`.`classID`',
+    conn.query('SELECT * FROM `nation` ; SELECT * FROM `product` JOIN `picture` ON `product`.`productID` = `picture`.`productID`',
         function (err, result) {
             res.render('index.ejs', { nation: result[0], product: result[1] });
         });
-
 })
 ///////////////網頁首頁//////////////////
 
@@ -51,3 +54,22 @@ app.get('/product/:ID', function (req, res) {
         });
 })
 //////////////指定商品頁/////////////////
+//INSERT INTO `buy` (`byID`, `userID`, `productID`, `productNUM`) VALUES (NULL, '1', '1', '1');
+/////////////新增商品到購物車/////////////
+app.get('/shopp', function (req, res) {
+    var userID = 1;
+    var productID = 1;
+    conn.query('INSERT INTO `buy` (`byID`, `userID`, `productID`, `productNUM`) VALUES (NULL, ?,?,1)',
+        [`${userID}`, `${productID}`],
+        function (err, field) {
+            if (err)
+                throw err;
+        })
+});
+
+app.get('/car', function (req, res) {
+    conn.query('SELECT * FROM `buy`',
+        function (err, result) {
+            res.render('shopp.ejs', { result });
+        })
+});
