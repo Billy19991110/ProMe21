7bf5eeb8744a2f6d109f1837b1b0166f1ffa8345
tea551234 <tea551234@gmail.com>
2022-03-07 15:30:31 +0800

    2022 0307 1530 ok table

--- app.js
+++ app.js
@@ -6,9 +6,7 @@ var bodyParser = require('body-parser');
 app.listen(3000);
 app.use(express.static('public'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
+app.use(bodyParser.urlencoded({ extended: false }));
 // app.use(bodyParser.urlencoded({ extended: false }));
 
 /* var bodyparser = require('bodyparser');
@@ -26,7 +24,7 @@ var conn = mysql.createConnection({
 
 });
 
-conn.connect(function(err) {
+conn.connect(function (err) {
     console.log(err);
 })
 
@@ -41,11 +39,11 @@ app.get('/', loggedIn, (req, res) => {
     WHERE `product`.`nationID` = 1 ORDER BY rand() LIMIT 4 ; \
     SELECT `product`.`productID`,`product`.`productName`,`product`.`productPrice`,`picture`.`pictureSeat1` FROM `product` \
     JOIN `picture` ON `product`.`productID` = `picture`.`productID` \
-    WHERE `product`.`nationID` = 2 ORDER BY rand() LIMIT 4";
+    WHERE `product`.`nationID` = 2 ORDER BY rand() LIMIT 4" ;
 
     if (req.user) {
         conn.query(sql,
-            function(err, result) {
+            function (err, result) {
                 res.render('index.ejs', {
                     japan: result[0],
                     korea: result[1],
@@ -55,7 +53,7 @@ app.get('/', loggedIn, (req, res) => {
             })
     } else {
         conn.query(sql,
-            function(err, result) {
+            function (err, result) {
                 res.render('index.ejs', {
                     japan: result[0],
                     korea: result[1],
@@ -68,10 +66,10 @@ app.get('/', loggedIn, (req, res) => {
 
 let sqlPage = "SELECT `product`.`productID`,`product`.`productName`,`product`.`productPrice`,`picture`.`pictureSeat1`, \
         (SELECT COUNT(*) FROM `product`) AS COUNT FROM `product` JOIN `picture` ON `product`.`productID` = `picture`.`productID` \
-        WHERE `product`.`nationID` = ? LIMIT ?,?";
+        WHERE `product`.`nationID` = ? LIMIT ?,?" ;
 
 //////////////日本頁/////////////////
-app.get('/japan/page:NUM', function(req, res) {
+app.get('/japan/page:NUM', function (req, res) {
     let pageNum = req.params.NUM;
     let start, end;
     if (pageNum == null) {
@@ -83,7 +81,7 @@ app.get('/japan/page:NUM', function(req, res) {
         end = 12;
     }
     conn.query(sqlPage, [1, start, end],
-        function(err, result) {
+        function (err, result) {
             res.render('japan.ejs', {
                 result,
                 status: 'loggedIn',
@@ -91,7 +89,7 @@ app.get('/japan/page:NUM', function(req, res) {
         });
 })
 //////////////韓國頁/////////////////
-app.get('/korea/page:NUM', function(req, res) {
+app.get('/korea/page:NUM', function (req, res) {
     let pageNum = req.params.NUM;
     let start, end;
     if (pageNum == null) {
@@ -103,7 +101,7 @@ app.get('/korea/page:NUM', function(req, res) {
         end = 12;
     }
     conn.query(sqlPage, [2, start, end],
-        function(err, result) {
+        function (err, result) {
             res.render('korea.ejs', {
                 result,
                 status: 'loggedIn',
@@ -112,30 +110,28 @@ app.get('/korea/page:NUM', function(req, res) {
 })
 
 //////////////指定商品頁/////////////////
-app.get('/product/:ID', function(req, res) {
+app.get('/product/:ID', function (req, res) {
     let sql = "SELECT * FROM `product`JOIN `picture` ON `product`.`productID` = `picture`.`productID` \
-                WHERE `product`.`productID` = ?";
+                WHERE `product`.`productID` = ?" ;
     let id = req.params.ID;
     conn.query(sql, [id],
-        function(err, result) {
+        function (err, result) {
             res.render('product.ejs', {
-                product: result,
-                status: 'loggedIn',
+                product: result, status: 'loggedIn',
             });
         }
     );
 })
 
-app.get('/shopp/:ID', function(req, res) {
-    let sql = "INSERT INTO `buy` (`byID`, `id`, `productID`, `productNUM`) VALUES (NULL, '7', ? , '1') ; \
+app.get('/shopp/:ID', function (req, res) {
+    let sql = "INSERT INTO `buy` (`byID`, `id`, `productID`, `productNUM`) VALUES (NULL, '1', ? , '1') ; \
                 SELECT * FROM`product`JOIN `picture` ON`product`.`productID` = `picture`.`productID` \
                 WHERE`product`.`productID` = ? "
     let id = req.params.ID;
     conn.query(sql, [id, id],
-        function(err, result) {
+        function (err, result) {
             res.render('product.ejs', {
-                product: result[1],
-                status: 'loggedIn',
+                product: result[1], status: 'loggedIn',
             })
         }
     );
@@ -143,48 +139,50 @@ app.get('/shopp/:ID', function(req, res) {
 
 //INSERT INTO `buy` (`byID`, `userID`, `productID`, `productNUM`) VALUES (NULL, '1', '1', '1');
 
-let sqlCart = "SELECT `buy`.`byID`, `buy`.`id`, `buy`.`productID`, `buy`.`productNUM`, `picture`.`pictureSeat1`, \
-                `product`.`productName`, `product`.`productPrice` FROM `buy` \
+let sqlCart = "SELECT `buy`.`byID`, `buy`.`id`, `buy`.`productID`, \
+                `buy`.`productNUM`, `picture`.`pictureSeat1`, `product`.`productName`, `product`.`productPrice` FROM `buy` \
                 JOIN `picture` ON `buy`.`productID` = `picture`.`productID`\
-                JOIN `product` ON `buy`.`productID` = `product`.`productID`";
+                JOIN `product` ON `buy`.`productID` = `product`.`productID`" ;
 //WHERE `buy`.`id` = ?";
 
-app.get('/data', function(req, res) {
+app.get('/data', function (req, res) {
     // let id = req.user.id;
     conn.query(sqlCart, [],
-        function(err, result) {
+        function (err, result) {
             var jsonString = JSON.stringify(result);
             res.send(jsonString);
         });
 });
-app.get("/cart", function(req, res) {
+app.get("/cart", function (req, res) {
     // let id = req.user.id;
     conn.query(sqlCart, [],
-        function(err, result) {
+        function (err, result) {
             res.render("cart.ejs", {
                 product: result,
                 status: 'loggedIn'
             });
         });
 });
-app.put("/cart", function(req, res) {
-    conn.query("update buy set productNUM = ? where byID = ?", [req.body.productNUM, req.body.byID],
-        function(err, rows) {
+app.put("/cart", function (req, res) {
+    conn.query("update buy set productNUM = ? where id = ?",
+        [],
+        function (err, rows) {
             res.send(JSON.stringify(req.body));
         });
 });
-app.delete("/cart", function(req, res) {
-    conn.query("delete from buy where byID = ?", [req.body.byID],
+app.delete("/cart", function (req, res) {
+    conn.query("delete from buy where byID = ?",
+        [req.body.byID],
         (err, result) => {
             res.send(JSON.stringify(req.body));
         });
 });
-app.get("/checkout", function(req, res) {
+app.get("/checkout", function (req, res) {
     res.render("checkout.ejs", {
         status: 'loggedIn'
     });
 });
-app.get("/checkout_2", function(req, res) {
+app.get("/checkout_2", function (req, res) {
     res.render("checkout_2.ejs", {
         status: 'loggedIn'
     });
@@ -248,19 +246,31 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(fileUpload());
 
 
-app.get('/wishList', routes.wishList); //call for main index page
-app.post('/', routes.wishList); //call for signup post 
-//Middleware
+app.get('/wishList', routes.wishList); 
+app.post('/', routes.wishList); 
 
-app.get('/todowishingPond', function(req, res) {
+app.get('/todowishingPond', function (req, res) {
     connection.query('SELECT * FROM `users_image`',
-        function(err, result) {
+        function (err, result) {
             res.render('todowishingPond.ejs', {
                 result,
                 status: 'loggedIn',
             });
         })
 });
+
+
+app.get('/customer_feedback', function (req, res) {
+    connection.query('SELECT * FROM `form`',
+        function (err, result) {
+            res.render('customer_feedback.ejs', {
+                result,
+                status: 'loggedIn',
+            });
+        })
+});
+
+
 // =====demo=====
 
 
