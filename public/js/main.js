fb5ce7bd530a5206388c33c3a6822a236bd428a7
billy1999110 <billy24681014@gmail.com>
2022-03-08 14:01:00 +0800

    0308_billy1400

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,6 @@
-$(function() {
+$(function () {
 
-	$.get("/data", function(e) {
+	$.get("/data", function (e) {
 		buyList = JSON.parse(e);
 		refreshNewsUI();
 	});
@@ -15,52 +15,52 @@ $(function() {
 			$("main").addClass("cartHeight");
 		}
 
-		$(".deleteButton").on("click", function() {
+		$(".deleteButton").on("click", function () {
 			var productIndex = $(this).closest(".oneProduct").index();
 			// console.log(productIndex);
 			// console.log(buyList[productIndex]);
 			$.ajax({
-					type: "delete",
-					url: "/cart",
-					data: buyList[productIndex],
-					success: function() {
-						Swal.fire({
-							icon: "info",
-							iconColor: "#89b0ae",
-							color: "#545b6e",
-							title: "商品已從購物車刪除",
-							html: "沒有喜歡的商品?去" + '<a href="todowishingPond">許願池</a>' + "看看",
-							confirmButtonColor: '#545b6e'
-						});
-					},
-					error: function() {
-						console.log("NO");
-					}
-				})
-				.then(function(e) {
-					$.get("/data", function(e) {
+				type: "delete",
+				url: "/cart",
+				data: buyList[productIndex],
+				success: function () {
+					Swal.fire({
+						icon: "info",
+						iconColor: "#89b0ae",
+						color: "#545b6e",
+						title: "商品已從購物車刪除",
+						html: "沒有喜歡的商品?去" + '<a href="todowishingPond">許願池</a>' + "看看",
+						confirmButtonColor: '#545b6e'
+					});
+				},
+				error: function () {
+					console.log("NO");
+				}
+			})
+				.then(function (e) {
+					$.get("/data", function (e) {
 						buyList = JSON.parse(e);
 						refreshNewsUI();
 					});
 				});
 			// window.location = "/cart";
 		});
-		$(".minusButton").on("click", function() {
+		$(".minusButton").on("click", function () {
 			var productIndex = $(this).closest(".oneProduct").index();
 			if (buyList[productIndex].productNUM > 1) {
 				buyList[productIndex].productNUM = buyList[productIndex].productNUM - 1;
 				$.ajax({
-						type: "put",
-						url: "/cart",
-						data: buyList[productIndex]
-					})
-					.then(function(e) {
-						$.get("/data", function(e) {
+					type: "put",
+					url: "/cart",
+					data: buyList[productIndex]
+				})
+					.then(function (e) {
+						$.get("/data", function (e) {
 							buyList = JSON.parse(e);
 							refreshNewsUI();
 						});
 					});
-				// window.location = "/cart";
+				window.location = "/cart";
 			} else {
 				Swal.fire({
 					icon: "error",
@@ -70,16 +70,16 @@ $(function() {
 				});
 			}
 		});
-		$(".plusButton").on("click", function() {
+		$(".plusButton").on("click", function () {
 			var productIndex = $(this).closest(".oneProduct").index();
 			buyList[productIndex].productNUM = buyList[productIndex].productNUM + 1;
 			$.ajax({
-					type: "put",
-					url: "/cart",
-					data: buyList[productIndex]
-				})
-				.then(function(e) {
-					$.get("/data", function(e) {
+				type: "put",
+				url: "/cart",
+				data: buyList[productIndex]
+			})
+				.then(function (e) {
+					$.get("/data", function (e) {
 						buyList = JSON.parse(e);
 						refreshNewsUI();
 					});
