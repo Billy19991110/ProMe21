c4f347acec729240365a641c50bdd6287b40e123
0range77 <owonce1127@gmail.com>
2022-03-09 16:03:46 +0800

    0309_丙醇v版1603

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ var conn = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
-    password: ''
+    password: 'root'
 
 });
 
@@ -227,7 +227,7 @@ const { compareSync } = require('bcryptjs');
 var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '',
+    password: 'root',
     database: 'test_v1'
 });
 
