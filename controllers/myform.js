7961efa90f58ff4d39efa9833a50f5d404a9eb22
billy1999110 <billy24681014@gmail.com>
2022-03-10 10:11:53 +0800

    0310_billy1011

--- controllers/myform.js
+++ controllers/myform.js
@@ -2,15 +2,14 @@ const db = require('../routes/db-config')
 
 
 const myform = async (req, res) => {
-    const{ email, productName , problem ,problemOptions} =req.body
-   
-//    console.log(typeof(req.body));
-//    console.log(typeof(productName));
-   
-    db.query('INSERT INTO form SET ?', { email: email, productName:productName  ,problem:problem , problemOptions:problemOptions}), async (err, result) => {
+    const { email, productName, problem, problemOptions } = req.body
+
+    //    console.log(typeof(req.body));
+    //    console.log(typeof(productName));
+
+    db.query('INSERT INTO form SET ?', { email: email, productName: productName, problem: problem, problemOptions: problemOptions }), async (err, result) => {
         if (err) throw err;
         return req.body
-       
     }
 }
 module.exports = myform
--- routes/index.js
+++ routes/index.js
@@ -1,45 +1,39 @@
 /*
 * GET home page.
 */
- 
-exports.wishList = function(req, res){
-    message = '';
-   if(req.method == "POST"){
-      var post  = req.body;
-      // console.log(post);
-      var email= post.email;
-      var name= post.name;
-      var commodity_name= post.commodity_name;
-      var Product_Description= post.Product_Description;
-      var count = 1
 
-	  if (!req.files)
-				return res.status(400).send('No files were uploaded.');
-
+exports.wishList = function (req, res) {
+	message = '';
+	if (req.method == "POST") {
+		var post = req.body;
+		// console.log(post);
+		var email = post.email;
+		var name = post.name;
+		var commodity_name = post.commodity_name;
+		var Product_Description = post.Product_Description;
+		var count = 1
+		if (!req.files)
+			return res.status(400).send('No files were uploaded.');
 		var file = req.files.uploaded_image;
-		var img_name=file.name;
+		var img_name = file.name;
+		if (file.mimetype == "image/jpeg" || file.mimetype == "image/png" || file.mimetype == "image/gif") {
+			file.mv('public/images/upload_images/' + file.name, function (err) {
+				if (err)
+					return res.status(500).send(err);
+				var sql = "INSERT INTO `users_image`(`email`,`name`,`commodity_name`,`Product_Description`, `image` , `count`) VALUES ('" + email + "','" + name + "','" + commodity_name + "','" + Product_Description + "','" + img_name + "','" + count + "')";
+				var query = db.query(sql, function (err, result) {
+					console.log(result.insertId);
+					res.redirect('todowishingPond');
+				});
+			});
+		} else {
+			message = "This format is not allowed , please upload file with '.png','.gif','.jpg'";
+			res.render('wishList.ejs', { message: message });
+		}
+	} else {
+		res.render('wishList');
+	}
 
-	  	 if(file.mimetype == "image/jpeg" ||file.mimetype == "image/png"||file.mimetype == "image/gif" ){
-                                 
-              file.mv('public/images/upload_images/'+file.name, function(err) {
-                             
-	              if (err)
-
-	                return res.status(500).send(err);
-      					var sql = "INSERT INTO `users_image`(`email`,`name`,`commodity_name`,`Product_Description`, `image` , `count`) VALUES ('" + email + "','" + name + "','" + commodity_name + "','" + Product_Description + "','"  + img_name + "','" + count + "')";
-    						var query = db.query(sql, function(err, result) {
-                         console.log(result.insertId);
-    							 res.redirect('todowishingPond');
-    						});
-					   });
-          } else {
-            message = "This format is not allowed , please upload file with '.png','.gif','.jpg'";
-            res.render('wishList.ejs',{message: message});
-          }
-   } else {
-      res.render('wishList');
-   }
- 
 };
 
 
@@ -47,9 +41,9 @@ exports.wishList = function(req, res){
 
 
 
-exports.todowishingPond = function(req, res){
-    var sql="SELECT * FROM `users_image` "; 
-    db.query(sql, function(err, result){
-      res.render('todowishingPond.ejs',{result});
-   });
+exports.todowishingPond = function (req, res) {
+	var sql = "SELECT * FROM `users_image` ";
+	db.query(sql, function (err, result) {
+		res.render('todowishingPond.ejs', { result });
+	});
 };
